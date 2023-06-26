35344ffa6d07a53c3c3abd58b481dd3e038b05a3
Bhavanisankar <shankar4137@gmail.com>
2023-06-26 15:29:30 +0530

    catalogue

--- shipping.sh
+++ shipping.sh
@@ -21,16 +21,14 @@ mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 echo -e "\e[31minstalling serivce\e[0m"
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service  &>>/tmp/roboshop.log
 
-echo -e "\e[31mRestarting services\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl start shipping &>>/tmp/roboshop.log
-
 echo -e "\e[31minstalling mysql\e[0m"
 yum install mysql -y  &>>/tmp/roboshop.log
 
 echo -e "\e[31mMysql passwordassigning\e[0m"
 mysql -h mysql-dev.devops89.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 
-echo -e "\e[31mRestarting\e[0m"
-systemctl restart shipping &>>/tmp/roboshop.log
+echo -e "\e[31mRestarting services\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl start shipping &>>/tmp/roboshop.log
+
