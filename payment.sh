9afe7df3c4d2f0b1508d32c7364c704195f6b4d8
Bhavanisankar <136574254+bhavanisankar-hub@users.noreply.github.com>
2023-06-26 13:40:50 +0530

    Update payment.sh

--- payment.sh
+++ payment.sh
@@ -1,1 +1,17 @@
+echo -e "\e[31menabling redis\e[0m"
+yum install python36 gcc python3-devel -y
 
+useradd roboshop
+
+mkdir /app 
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip 
+cd /app 
+unzip /tmp/payment.zip
+
+cd /app 
+pip3.6 install -r requirements.txt
+
+systemctl daemon-reload
+systemctl enable payment 
+systemctl start payment
