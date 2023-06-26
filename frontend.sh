4ee757596faa815020df0c3a340c1eaecd8f6958
Bhavanisankar <shankar4137@gmail.com>
2023-06-26 08:47:46 +0530

    catalogue

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,18 @@
 echo -e "\e[31mInstalling nginx server\e[0m"
-yum install nginx -y >/tmp/roboshop.log
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[31mremoving old content\e[0m"
-rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[31mdownloading content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
 echo -e "\e[31mextracting nginx server\e[0m"
-cd /usr/share/nginx/html >/tmp/roboshop.log
-unzip /tmp/frontend.zip  >/tmp/roboshop.log
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
 
 ##need to configure fine
 
 echo -e "\e[31mrestarting nginx server\e[0m"
-systemctl enable nginx   >/tmp/roboshop.log
-systemctl restart nginx  >/tmp/roboshop.log
+systemctl enable nginx   &>>/tmp/roboshop.log
+systemctl restart nginx  &>>/tmp/roboshop.log
