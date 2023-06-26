bcb65b871c4256fdbfab74e5b2603a7ba3065e4f
Bhavanisankar <shankar4137@gmail.com>
2023-06-26 09:59:25 +0530

    catalogue

---
+++ catalogue.sh
@@ -0,0 +1,38 @@
+echo -e "\e[31mconfiguring node js repo\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
+
+echo -e "\e[31minstallingnode js\e[0m"
+yum install nodejs -y  &>>/tmp/roboshop.log
+
+echo -e "\e[31madding dir\e[0m"
+useradd roboshop  &>>/tmp/roboshop.log
+
+echo -e "\e[31mremoving old one and creating new appdir\e[0m"
+rm -rf /app  &>>/tmp/roboshop.log
+mkdir /app   &>>/tmp/roboshop.log
+
+echo -e "\e[31mextracting content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+cd /app  &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip   &>>/tmp/roboshop.log
+
+echo -e "\e[31installing o\e[0m"
+cd /app &>>/tmp/roboshop.log
+npm install   &>>/tmp/roboshop.log
+
+echo -e "\e[31minstalling serivce\e[0m"
+cp catalogue.servie /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+
+echo -e "\e[31mrestrating\e[0m"
+systemctl daemon-reload   &>>/tmp/roboshop.log
+systemctl enable catalogue   &>>/tmp/roboshop.log
+systemctl start catalogue   &>>/tmp/roboshop.log
+
+echo -e "\e[31copymmanodb repo\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo  &>>/tmp/roboshop.log
+
+echo -e "\e[31minstalling mango db\e[0m"
+yum install mongodb-org-shell -y   &>>/tmp/roboshop.log
+
+echo -e "\e[31mload mangodb\e[0m"
+mongo --host mongodb-dev.devops89.site </app/schema/catalogue.js  &>>/tmp/roboshop.log
