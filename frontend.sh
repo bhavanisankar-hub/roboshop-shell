8ce0a18e1673eff497860151f384e4c7f0ff0d9c
Bhavanisankar <shankar4137@gmail.com>
2023-06-26 08:44:30 +0530

    catalogue

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[31mremoving old content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[31mdownloading content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log >/tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
 
 echo -e "\e[31mextracting nginx server\e[0m"
 cd /usr/share/nginx/html >/tmp/roboshop.log
