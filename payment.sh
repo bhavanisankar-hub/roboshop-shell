355040b8469e87ff9dd93e301a549f2fb4c5ec5d
Bhavanisankar <136574254+bhavanisankar-hub@users.noreply.github.com>
2023-06-26 13:46:10 +0530

    Update payment.sh

--- payment.sh
+++ payment.sh
@@ -1,17 +1,26 @@
 echo -e "\e[31menabling redis\e[0m"
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y  &>>/tmp/roboshop.log
 
-useradd roboshop
+echo -e "\e[31museradding\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
 
-mkdir /app 
+echo -e "\e[31mmaking directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app  &>>/tmp/roboshop.log
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip 
-cd /app 
-unzip /tmp/payment.zip
+echo -e "\e[31mExtracting file\e[0m"
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip  &>>/tmp/roboshop.log
+cd /app  &>>/tmp/roboshop.log
+unzip /tmp/payment.zip  &>>/tmp/roboshop.log
 
-cd /app 
-pip3.6 install -r requirements.txt
+echo -e "\e[31mDownloading dependencies\e[0m"
+cd /app   &>>/tmp/roboshop.log 
+pip3.6 install -r requirements.txt  &>>/tmp/roboshop.log
 
-systemctl daemon-reload
-systemctl enable payment 
-systemctl start payment
+echo -e "\e[31minstalling serivce\e[0m"
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service  &>>/tmp/roboshop.log
+
+echo -e "\e[31mRestarting\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable payment  &>>/tmp/roboshop.log
+systemctl start payment  &>>/tmp/roboshop.log
